7c0ff6aac466e417a30fcb16974ae88b61dec547
Nuno Fernandes <nuno.fernandes@inflightit.com>
2022-09-24 22:16:59 +0100

    done

--- src/containers/404.tsx
+++ src/containers/404.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Lottie from "react-lottie";
+
+import * as error from "../assets/error404.json"
+
+import { Box } from "@mui/material";
 
 export default function Error404() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: error,
+        name: "error",
+    };
+
     return (
-        <React.Fragment>
-            <h1>ERROR - SORRY</h1>
-        </React.Fragment>
+        <Box display="flex"
+            justifyContent="center"
+            alignItems="center"
+            style={{ position: "absolute", top: 0, bottom: 0 }}
+        >
+            <Lottie options={defaultOptions} height={600} width={600} />
+        </Box>
     );
 }
