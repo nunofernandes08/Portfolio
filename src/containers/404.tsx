be7ba99db74a1a403804c81b81a0280906089e50
Nuno Fernandes <nuno.fernandes@inflightit.com>
2022-09-24 18:56:19 +0100

    test deploy 2

--- src/containers/404.tsx
+++ src/containers/404.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Error404() {
     return (
         <React.Fragment>
-            <h1>ERROR</h1>
+            <h1>ERROR - SORRY</h1>
         </React.Fragment>
     );
 }
